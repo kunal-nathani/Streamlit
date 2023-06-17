779affef1dc9baab34adf04c80ebecffa5e12386
kunal-nathani <48233889+kunal-nathani@users.noreply.github.com>
2023-06-17 13:20:28 +0530

    Add files via upload

---
+++ Text Element.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("I am Title")
+st.header("I am Header")
+st.subheader("I am Sub Header")
+st.markdown("I am Markdown")
+st.code("I am code block.Eg - int a = 5")
+st.text("I am Text")
+st.write("I am write")
